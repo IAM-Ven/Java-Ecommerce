092eea814d8d2724db6888d7ebd771f6099bd3da
eyoeldefare <edefare@masonlive.gmu.edu>
2018-03-17 22:59:27 -0400

    Finished creating a home page for the project.

--- javaecom/src/main/java/net/eyoel/javaecom/controller/PageController.java
+++ javaecom/src/main/java/net/eyoel/javaecom/controller/PageController.java
@@ -1,12 +1,19 @@
 package net.eyoel.javaecom.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	
-	
+	/*
+	 * This will display the page.jsp content as long as it maps to the / and /home
+	 * params
+	 */
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Wellcome");
+		return mv;
+	}
 }
